2b416c8af5d073d467f5dd937a26d50d09e563af
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-09 10:44:24 +0530

    routes added

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/import_excel', 'ImportExcelController@index');
-Route::post('/import_excel/import', 'ImportExcelController@import');
+Route::get('/', 'ExcelController@index');
+
+Route::post('/import', 'ExcelController@importData');
+
+Route::get('/export', 'ExcelController@exportData');
